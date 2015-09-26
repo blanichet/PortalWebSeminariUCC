158d198b452bc599d776c746ca47b1cab77d72ea
blanichet <cindyb.parra@campusucc.edu.co>
2015-09-26 13:29:45 -0500

    se sube burbujas chat con hora

--- js/chat/chat.js
+++ js/chat/chat.js
@@ -1,6 +1,6 @@
 $(function(){
 
-var objFirebase = new Firebase("https://chatucc.firebaseio.com/");
+var objFirebase = new Firebase("https://chatucccbps.firebaseio.com/");
 	$('input[type=submit]').click(clickEnvio);
 		
 	
@@ -8,9 +8,15 @@ var objFirebase = new Firebase("https://chatucc.firebaseio.com/");
 	function clickEnvio(){
 		var mensaje = $('#inMensaje').val();
 		
+		var currentdate = new Date();
+
+    var datetime = currentdate.getHours() + ":" 
+    + currentdate.getMinutes() + ":" + currentdate.getSeconds();
 		objFirebase.push({
            autor: "CindyB",
+           id: 2,
            mensaje: mensaje,
+           hora: datetime,
         
   }); 
 		console.log(mensaje);
@@ -18,14 +24,37 @@ var objFirebase = new Firebase("https://chatucc.firebaseio.com/");
 
 	objFirebase.on("child_added", function(data){
 		var registro = data.val();
-		$('.cont-mensajes-timeline').prepend(getPantilla(registro.autor,registro.mensaje));
+		$('.cont-mensajes-timeline').prepend(getPantilla(registro.autor,registro.mensaje,registro.id,registro.hora));
 	});
 
-	function getPantilla(autor,mensaje){
-		var plantilla = '<div class="cont-mensajes-mensaje">\
-		<label for="" id="lblmensaje">'+autor +'-->'+ mensaje + '</label> \
-		</div>';
+	function getPantilla(autor,mensaje,id,hora){
 
-		return plantilla;
+		/*var plantilla = '<center><div class="cont-mensajes-mensaje">\
+		<label for="" id="lblmensaje">'+autor +'-->'+ mensaje + '</label> \
+		</div></center>';
+
+		return plantilla;*/
+		var mensajes = mensaje;
+		var emo= mensajes.replace(":)", '<img class="izquierda"/>');
+		var emo= emo;
+		var emo= emo.replace(":(", '<img class="izquierda" />');
+
+		if(id!=2){
+		var plantilla = '<center><div class="chat-bubble">\
+                  <span class="span-mensaje">'+ emo + '</span>\
+                    <span class="span-hora">'+ hora + '</span>\
+                   <div class="chat-bubble-arrow-border"></div>\
+                   <div class="chat-bubble-arrow"></div>\
+            </div></center>';
+        }
+        else{
+        	var plantilla = '<center><div class="chat-bubble-right">\
+                  <span class="span-mensaje-right">'+ emo + '</span>\
+                    <span class="span-hora-right">'+ hora + '</span>\
+                   <div class="chat-bubble-arrow-border-right"></div>\
+                   <div class="chat-bubble-arrow-right"></div>\
+            </div></center>';
+        }
+        return plantilla;
 	}
 });
